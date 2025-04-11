f35c44336c8362be1417717b6a1efcda68bfd12c
Lokendra Tiwari <lokendratiwari22844@gmail.com>
2025-04-11 17:46:30 +0530

    Reaction-Reducer

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,8 +1,7 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
-
+import {sub} from "date-fns";
 const initialState = [
-    { id: '1', title: 'Learning Redux Toolkit', content: "I've heard good things" },
-    { id: '2', title: 'Slices...', content: "The more I say slice,the more I want pizza" },
+
 ]
 
 const postsSlice = createSlice({
@@ -10,24 +9,38 @@ const postsSlice = createSlice({
     initialState,
     reducers: {
         postAdded: {
-            reducer(state, action)
-            {
+            reducer(state, action) {
                 state.push(action.payload);
             },
-            prepare(title,content,userId) {
+            prepare(title, content, userId) {
                 return {
-                    payload : {
+                    payload: {
                         id: nanoid(),
                         title,
                         content,
-                        userId
+                        date: new Date().toISOString(),
+                        userId,
+                        reactions:{
+                            thumbsUp:0,
+                            wow:0,
+                            heart:0,
+                            rocket:0,
+                            coffee:0,
+                        }
                     }
                 }
+            }
+        },
+        reactionAdded(state,action){
+            const {postId,reaction} = action.payload;
+            const existingPost = state.find(post=>post.id === postId)
+            if(existingPost){
+                existingPost.reactions[reaction]++;
             }
         }
     }
 })
 
 export const selectAllPosts = (state) => state.posts;
-export const { postAdded } = postsSlice.actions;
+export const { postAdded,reactionAdded } = postsSlice.actions;
 export default postsSlice.reducer
